a8cfc5a88df91b5dc11a8b9eace4e5296b06ac9c
tbenz9 <tbenz9@gmail.com>
2013-11-04 12:42:03 -0700

    removed Integers and auto_increments from DDL

--- DDL.sql
+++ DDL.sql
@@ -18,9 +18,9 @@
 -- Create the employees Table
 CREATE TABLE employees
 (
-	employees_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	employees_ID VARCHAR(10) PRIMARY KEY NOT NULL,
 	title VARCHAR(100),
-	manager INTEGER,
+	manager VARCHAR(10),
 	office_number VARCHAR(30),
 	hire_date DATE,
 	phone_number VARCHAR(20)
@@ -29,31 +29,31 @@ CREATE TABLE employees
 -- Create the computers Table
 CREATE TABLE computers
 (
-	computers_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	computers_ID VARCHAR(10) PRIMARY KEY NOT NULL,
 	operating_system VARCHAR(30),
 	motherboard VARCHAR(30),
 	ram VARCHAR(30),
 	assignment_date DATE,
 	upgrade_date DATE,
-	employees_ID INTEGER
+	employees_ID VARCHAR(10)
 );
 
 -- Create the macaddresses Table
 CREATE TABLE macaddresses
 (
 	macaddresses_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	computers_ID INTEGER,
+	computers_ID VARCHAR(10),
 	mac_address VARCHAR(23)
 );
 
 -- Create the harddrives Table
 CREATE TABLE harddrives
 (
-	harddrives_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	harddrives_ID VARCHAR(10) PRIMARY KEY NOT NULL,
 	capacity INTEGER,
 	serial_number VARCHAR(20),
 	model VARCHAR(20),
-	computers_ID INTEGER
+	computers_ID VARCHAR(10)
 );
 
 -- Create the licenses Table
@@ -65,13 +65,13 @@ CREATE TABLE licenses
 	max_activation TINYINT,
 	license_name VARCHAR(45),
 	license_expiration_date DATE,
-	computers_ID INTEGER
+	computers_ID VARCHAR(10)
 );
 
 -- Create the switches Table
 CREATE TABLE switches
 (
-	switches_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	switches_ID VARCHAR(10) PRIMARY KEY NOT NULL,
 	password VARCHAR(30),
 	username VARCHAR(30),
 	location VARCHAR(30),
@@ -83,9 +83,9 @@ CREATE TABLE switches
 -- Create the ports Table
 CREATE TABLE ports
 (
-	switches_ID INTEGER,
+	switches_ID VARCHAR(10),
 	port_number TINYINT,
-	device_ID INTEGER
+	device_ID VARCHAR(10)
 );
 
 -- Create the warranties Table
@@ -96,44 +96,44 @@ CREATE TABLE warranties
 	vendor VARCHAR(30),
 	warranty_length VARCHAR(20),
 	purchase_date DATE,
-	devices_ID INTEGER
+	devices_ID VARCHAR(10)
 );
 
 -- Create the monitors Table
 CREATE TABLE monitors
 (
-	monitors_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	monitors_ID VARCHAR(10) PRIMARY KEY NOT NULL,
 	serial_number VARCHAR(20),
 	model VARCHAR(30),
 	screen_size TINYINT,
-	computers_ID INTEGER
+	computers_ID VARCHAR(10)
 );
 
 -- Create the devices Table
 CREATE TABLE devices
 (
-	devices_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	devices_ID VARCHAR(10) PRIMARY KEY NOT NULL,
 	serial_number VARCHAR(20),
 	model VARCHAR(30),
 	devices_type VARCHAR(20),
-	employees_ID INTEGER
+	employees_ID VARCHAR(10)
 );
 
 -- Create the phones Table
 CREATE TABLE phones
 (
-	phones_ID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	phones_ID VARCHAR(10) PRIMARY KEY NOT NULL,
 	serial_number VARCHAR(20),
 	model VARCHAR(30),
 	ip VARCHAR(15),
 	switch_port VARCHAR(20),
-	empoyees_ID INTEGER
+	employees_ID VARCHAR(10)
 );
 
 -- Create the reviews Table
 CREATE TABLE reviews
 (
-	reviewer INTEGER PRIMARY KEY NOT NULL,
+	reviewer VARCHAR(10) PRIMARY KEY NOT NULL,
 	review_date DATE,
 	rating INTEGER
 );
